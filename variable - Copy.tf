fbee280521d28737110fe28bbfcd43b0fd3efee0
swsrivas <136589049+swsrivas@users.noreply.github.com>
2023-06-14 16:32:15 +0530

    Add files via upload

---
+++ variable - Copy.tf
@@ -0,0 +1,83 @@
+variable "project" {
+  type = string
+  default = "prj-1804-51958669-nsvc"
+}
+
+variable "region" {
+  type = string
+  default = "europe-west1"
+}
+
+variable "zone" {
+  type = string
+  default = "europe-west1-b"
+}
+
+variable "vpc" {
+  type = string
+  default = "demovpc"
+}
+
+variable "backsubnet" {
+  type = string
+  default = "backsubnet"
+}
+
+variable "proxysubnet" {
+  type = string
+  default = "proxysubnet"
+}
+
+variable "credentials" {
+  type = string
+  default = "/home/swati_srivastava/newdemo.json"
+}
+
+variable "instance-template-tf" {
+  type = string
+  default = "testtemplate"
+}
+
+variable "machine_type" {
+  type = string
+  default = "e2-micro"
+}
+
+variable "autohealing-healthcheck" {
+  type = string
+  default = "autohealing-health-check"
+}
+
+variable "instance-group-manager" {
+  type = string
+  default = "instance-group-manager"
+}
+
+variable "autoscaler" {
+  type = string
+  default = "autoscaler"
+}
+
+variable "subnet" {
+  type = string
+  default = "testsubnet"
+}
+
+variable "httpproxy" {
+  type = string
+  default = "http-proxy"
+}
+
+
+
+variable "urlmap" {
+  type = string
+  default = "load-balancer"
+}
+
+
+
+variable "backend" {
+  type = string
+  default = "backend"
+}
